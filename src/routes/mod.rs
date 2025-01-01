da9dcddd08771169bc00ee89424809a0b427f176
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-01 18:22:21 +0100

    feat: sending kafka message throught sse

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,10 +3,17 @@ use axum::{
     routing::get,
     Router,
 };
-use tokio_stream::StreamExt as _;
 use axum_extra::{headers::UserAgent, TypedHeader};
 use futures::stream::{self, Stream};
-use std::{convert::Infallible, time::Duration};
+use kafka::Error;
+use std::{
+    convert::Infallible,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+use tokio_stream::StreamExt as _;
+
+use crate::messaging::{process_by_client, stream_kafka_events, stream_kafka_events_rdkafka};
 
 pub fn create_route() -> Router {
     Router::new()
@@ -22,18 +29,9 @@ pub async fn sse_handler(
     TypedHeader(user_agent): TypedHeader<UserAgent>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     println!("`{}` connected", user_agent.as_str());
-
-    // A `Stream` that repeats an event every second
-    //
-    // You can also create streams from tokio channels using the wrappers in
-    // https://docs.rs/tokio-stream
-    let stream = stream::repeat_with(|| Event::default().data("hi!"))
-        .map(Ok)
-        .throttle(Duration::from_secs(1));
-
-    Sse::new(stream).keep_alive(
-        axum::response::sse::KeepAlive::new()
-            .interval(Duration::from_secs(1))
-            .text("keep-alive-text"),
-    )
+    let rx_stream = stream_kafka_events_rdkafka();
+    Sse::new(rx_stream).keep_alive(axum::response::sse::KeepAlive::new()
+        .interval(Duration::from_secs(15))
+        .text("keep-alive-text"))
 }
+
