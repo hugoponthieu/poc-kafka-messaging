1db5ba995d85d6ca75dceaeed720ce208f9a792b
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-01-02 16:23:01 +0100

    feat:added services

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct AppConfig {
-    #[arg(long, env = "KAFKA_HOST", default_value = "localhost:9092")]
+    #[arg(long, env = "KAFKA_HOST", default_value = "127.0.0.1:9092")]
     pub kafka_host: String,
 
     #[arg(long, env = "HTTP_HOST", default_value = "localhost")]
@@ -13,16 +13,32 @@ pub struct AppConfig {
     pub http_port: u16,
 }
 
-impl AppConfig {
-    pub fn builder() -> AppConfig {
+pub trait AppConfigTrait {
+    fn builder() -> AppConfig;
+
+    fn make() -> AppConfig;
+
+    fn set_kafka_host(&mut self, host: String);
+
+    fn set_http_host(&mut self, host: String);
+
+    fn set_http_port(&mut self, port: u16);
+
+    fn default() -> Self;
+
+    fn log_config(&self);
+}
+
+impl AppConfigTrait for AppConfig {
+    fn builder() -> AppConfig {
         AppConfig {
             kafka_host: String::new(),
             http_host: String::new(),
             http_port: 0,
         }
     }
-    
-    pub fn make() -> AppConfig {
+
+    fn make() -> AppConfig {
         let args = AppConfig::parse();
         let config = AppConfig {
             kafka_host: args.kafka_host,
@@ -33,19 +49,19 @@ impl AppConfig {
         config
     }
 
-    pub fn set_kafka_host(&mut self, host: String) {
+    fn set_kafka_host(&mut self, host: String) {
         self.kafka_host = host;
     }
 
-    pub fn set_http_host(&mut self, host: String) {
+    fn set_http_host(&mut self, host: String) {
         self.http_host = host;
     }
 
-    pub fn set_http_port(&mut self, port: u16) {
+    fn set_http_port(&mut self, port: u16) {
         self.http_port = port;
     }
 
-    pub fn default() -> Self {
+    fn default() -> Self {
         AppConfig {
             kafka_host: "localhost:9092".to_string(),
             http_host: "localhost".to_string(),
@@ -53,7 +69,7 @@ impl AppConfig {
         }
     }
 
-    pub fn log_config(&self) {
+    fn log_config(&self) {
         tracing::info!("Kafka Host: {}", self.kafka_host);
         tracing::info!("HTTP Host: {}", self.http_host);
         tracing::info!("HTTP Port: {}", self.http_port);
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,19 +1,7 @@
-use axum::{
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 
 use crate::messaging::messages_controller::sse_handler;
 
-
 pub fn create_route() -> Router {
-    Router::new()
-        .route("/sse", get(sse_handler))
-        .route("/", get(health_handler))
+    Router::new().route("/sse", get(sse_handler))
 }
-
-pub async fn health_handler() -> impl IntoResponse {
-    "OK"
-}
-
